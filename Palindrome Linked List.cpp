2783afb594b00a87c16005bdf4dfe9cf768388d3
lamho866 <72925954+lamho866@users.noreply.github.com>
2020-11-27 00:16:23 +0800

    Add files via upload

---
+++ Palindrome Linked List.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+        //take all element in the singly-linked list.
+        while(head != nullptr)
+            vtr.push_back(head->val), head = head -> next;
+        j = vtr.size() - 1;
+        
+        //check it whether is Palindrome
+        while(i < j)
+        {
+            if(vtr[i] != vtr[j])
+                return false;
+            ++i, --j;
+        }
+        
+        return true;
+    }
+private:
+    int i = 0, j = 0;
+    vector<int> vtr; //save the singly-linked element
+};
+//step 1: take all element in the singly-linked list.
+//setp 2: check it whether is Palindrome.
