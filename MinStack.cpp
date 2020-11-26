1d905f1bc4b4e193b9399b555b2b7c0e8da54c46
lamho866 <72925954+lamho866@users.noreply.github.com>
2020-11-27 00:13:08 +0800

    Add files via upload

---
+++ MinStack.cpp
@@ -0,0 +1,69 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        stk.push_back(x);
+         //case 1: if minStk doesn't have any element, then push it
+         //case 2: if x is minimum element or equal push it in minStk
+        if(minStk.empty() || x <= minStk.back())
+            minStk.push_back(x);
+    }
+    
+    void pop() {
+        //update the minimum element
+        if(stk.back() == minStk.back())
+             minStk.pop_back();
+        stk.pop_back();
+    }
+    
+    int top() {
+        return stk.back();
+    }
+    
+    int getMin() {
+        return  minStk.back();
+    }
+    
+private:
+    vector<int> stk; // save the stack value
+    vector<int> minStk;// save the current minimum value on the stack
+};
+/*
+Example :
+stk : 5 2 2 3
+minstk : 2 2
+
+push(1)
+
+stk : 5 2 2 3 1
+minstk : 2 2 1
+
+pop()
+
+stk : 5 2 2 3
+minstk : 2 2
+
+pop()
+
+stk : 5 2 2
+minstk : 2 2
+
+pop() <=  if we don't push the same minimum element, it will happens error
+
+stk : 5 2
+minstk : 2 
+
+*/
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
